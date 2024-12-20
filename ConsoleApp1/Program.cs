a72485f9d09c7c580c48e9aa22372836128b9caf
Rashmi R S <rashmirangana.surpu@hcl.com>
2024-12-20 17:44:59 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// The main entry point for the console application. This class contains the `Main` method that is executed first when the program is run. 
+    /// </summary>
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Create a object of Student class from Student.cs file and print details of the student
+            Student student = new Student();
+            student.PrintDetails();
+
+
+            Console.ReadLine();
+        }
+
+
+        //Code Starts here
+
+        public int AddAndPrint(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine($"El resultado de sumar {num1} y {num2} es: {result}");
+            return result;
+        }
+       
+
+
+
+
+
+
+
+        public void PrintNumbers()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i < 5)
+                    Console.WriteLine(i);
+                else
+                    Console.WriteLine(i + 1);
+
+            }
+        }
+    }
+}
+
+
+
+
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    //define the class Student with Rollno, Name , Semister, Address as filelds , include 
+    //default constructor to initalize all fields to default values
+    public class Student
+    {
+        public int Rollno { get; set; }
+        public string Name { get; set; }
+        public int Semester { get; set; }
+        public string Address { get; set; }
+
+        //Default constructor
+        public Student()
+        {
+            Rollno = 100;
+            Name = "Student1";
+            Semester = 1;
+            Address = "";
+        }
+
+        //write a function to print details os a student
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Roll number: {Rollno}\nName: {Name}\nSemester: {Semester}\nAddress: {Address}");
+        }
+    }
+
+
+}
+
